44188c05a4c075476e4e3b22877248a5b4bb50c6
Sergey <platout_land@mail.ru>
2021-07-22 21:31:13 +0300

    commit2

--- lesson3.py
+++ lesson3.py
@@ -1,1 +1,38 @@
-print ('lesson3')
+# l = list ()
+# l.append ('ertey')
+# l.append ('10')
+# l.pop()
+# print (l)
+
+# a = ('abcde',)
+# a = [0]
+# print (type(a))
+
+# d= {1: 12345}
+# print (d)
+# d= {'a': 12345}
+# print (d)
+# d = dict (a=1234, b=3245,c=678)
+# print (d)
+# print (d['a'])
+# print (d.keys())
+# s = set([12 , 34 , 56])
+# print (s)
+# print(len (s))
+# a = 6 
+# if a > 5:
+#     print('bigger')
+# else:
+#     print('smaller') 
+# shop = {
+#     'milk':2,
+#     'butter':5,
+#     'muka':7,
+# }
+# a = input()
+# print(shop.get(a, -1))
+l = [1, 17, 7, 14, 49, 7,]
+
+for x in l:
+    if x % 7 == 0:
+        print(x)
